efbcbc466dc17acbd210e89bd1aabcdadde2fd64
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-06-04 14:57:30 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+require('./bot/index.js');
+require("./keep_alive.js");
+
+process.on('uncaughtException', console.error);
+process.on('unhandledRejection', console.error);
+
+/*
+* @Author
+* Whattyu
+*/
