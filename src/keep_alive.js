7373be80c33fe1103eef0b64bd358c4b2c8bedbb
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-06-04 14:58:08 +0300

    Create keep_alive.js

---
+++ src/keep_alive.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080)
+app.get('/', function (req,res) {
+	res.send("Naber ben Cariye+ burasi da hicbir ishe yaramayan sayfam Made by: Whattyu#8272");
+});
