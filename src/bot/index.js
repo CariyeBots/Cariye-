c6da1d7e784ee1a52df295439567726d6061a961
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-07-21 18:19:39 +0300

    Update index.js

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,32 +1,25 @@
-require('dotenv').config();
-require("../keep_alive.js");
-require('@gcommands/plugin-moreevents');
-const { Intents, Collection } = require('discord.js');
-const { join } = require('path');
-const { readFileSync } = require('fs');
-const { GClient, Plugins, Logger, Command, Component } = require("gcommands");
 const { MongoDBProvider } = require('gcommands/dist/providers/MongoDBProvider');
+const { GClient, Plugins, Logger, Command, Component } = require("gcommands");
+const { GiveawaysManager } = require('discord-giveaways');
+const { Intents, Collection } = require('discord.js');
+const { readFileSync } = require('fs');
 const mongoose = require("mongoose");
+const { join } = require('path');
+require('@gcommands/plugin-language').default({ defaultLanguage: 'en-GB', languageText: JSON.parse(readFileSync(`${__dirname}/responses.json`, 'utf-8')) });
+require('@gcommands/plugin-votes').default({ type: 'TOP.GG', apiKeys: process.env.topgg, serverAuthKey: process.env.topggwh });
+require('@gcommands/plugin-moreevents');
+require("../keep_alive.js");
+require('dotenv').config();
+
 Plugins.search(__dirname);
-
-require('@gcommands/plugin-language').default({
-  defaultLanguage: 'en-GB',
-  languageText: JSON.parse(readFileSync(`${__dirname}/responses.json`, 'utf-8'))
-});
-
-require('@gcommands/plugin-votes').default({
-  type: 'TOP.GG',
-  apiKeys: process.env.topgg,
-  serverAuthKey: process.env.topggwh
-});
 
 Command.setDefaults({ cooldown: '5s' });
 
 Component.setDefaults({
-  onError: (ctx, error) => {
+	onError: (ctx, error) => {
     console.log(error)
-    return ctx.reply('Oops! Something went wrong')
-  }
+		return ctx.reply('Oops! Something went wrong')
+	}
 });
 
 Logger.setLevel(Logger.TRACE);
@@ -35,13 +28,13 @@ const client = new GClient({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Intents.FLAGS.GUILD_BANS,
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
     Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_PRESENCES,
     Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
     Intents.FLAGS.GUILD_INVITES,
     Intents.FLAGS.GUILD_INTEGRATIONS,
     Intents.FLAGS.GUILD_PRESENCES,
@@ -50,14 +43,29 @@ const client = new GClient({
     join(__dirname, 'commands'),
     join(__dirname, 'events')
   ],
+	failIfNotExists: true,
   database: new MongoDBProvider(process.env.mongodb_uri),
-  shards: "auto",
-  messageSupport: true,
-  messagePrefix: '!',
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+	messageSupport: true,
+	messagePrefix: '!',
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
 client.queue = new Collection();
+client.db = new Collection();
+client.giveawaysManager = new GiveawaysManager(client, {
+  storage: "./giveaways.json",
+  default: {
+    botsCanWin: false,
+    embedColor: "#FF0000",
+    reaction: "🎉",
+    lastChance: {
+      enabled: true,
+      content: '⚠️ **LAST CHANCE TO ENTER !** ⚠️',
+      threshold: 5000,
+      embedColor: '#FF0000'
+    }
+  }
+});
 
 mongoose.connect(process.env.mongodb_uri, {
   useNewUrlParser: true,
@@ -68,11 +76,16 @@ client.on('error', console.log);
 client.on('ratelimit', console.log);
 client.on('warn', console.log);
 
-client.login(process.env.token);
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageId} (${reaction.emoji.name})`);
+});
 
-/*
-* I'll love the light for it shows me the way, yet I'll endure the darkness because it shows me the stars.
-*
-* @Author
-* Whattyu
-*/
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageId} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageId} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
+
+client.login(process.env.token);
