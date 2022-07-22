14fd0348c99892af32df2e58a101fdfc2b03b008
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-07-22 13:33:05 +0300

    Update ai.js

--- src/bot/commands/ai.js
+++ src/bot/commands/ai.js
@@ -1,5 +1,4 @@
 const { Command, CommandType, Argument, ArgumentType } = require("gcommands")
-//const fetch = require("node-fetch")
 const { Configuration, OpenAIApi } = require("openai");
 
 new Command({
@@ -29,11 +28,7 @@ new Command({
   async run({ reply, arguments }) {
     let text = arguments.getString('text')
 
-		/*const fetched = await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(text)}&botname=${encodeURIComponent('Cariye+')}&ownername=${encodeURIComponent('whattyu')}&user=${encodeURIComponent(member.user.username)}`, {});
-		
-		let response = await fetched.json();*/
-
-    const configuration = new Configuration({
+		const configuration = new Configuration({
   		apiKey: process.env.openai,
 		});
 		const openai = new OpenAIApi(configuration);
