3ea465ea14571ab9f90bf6820b198227bb5e5029
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-07-30 22:22:55 +0300

    Update to V14

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,13 +1,11 @@
 const { MongoDBProvider } = require('gcommands/dist/providers/MongoDBProvider');
 const { GClient, Plugins, Logger, Command, Component } = require("gcommands");
-const { GiveawaysManager } = require('discord-giveaways');
-const { Intents, Collection } = require('discord.js');
+const { Collection, GatewayIntentBits, Partials } = require('discord.js');
 const { readFileSync } = require('fs');
 const mongoose = require("mongoose");
 const { join } = require('path');
 require('@gcommands/plugin-language').default({ defaultLanguage: 'en-GB', languageText: JSON.parse(readFileSync(`${__dirname}/responses.json`, 'utf-8')) });
 require('@gcommands/plugin-votes').default({ type: 'TOP.GG', apiKeys: process.env.topgg, serverAuthKey: process.env.topggwh });
-require('@gcommands/plugin-moreevents');
 require('dotenv').config();
 
 Plugins.search(__dirname);
@@ -15,76 +13,52 @@ Plugins.search(__dirname);
 Command.setDefaults({ cooldown: '5s' });
 
 Component.setDefaults({
-  onError: (ctx, error) => {
+	onError: (ctx, error) => {
     console.log(error)
-    return ctx.reply('Oops! Something went wrong')
-  }
+		return ctx.reply('Oops! Something went wrong')
+	}
 });
 
 Logger.setLevel(Logger.TRACE);
 
 const client = new GClient({
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_BANS,
-    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.DIRECT_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Intents.FLAGS.GUILD_INVITES,
-    Intents.FLAGS.GUILD_INTEGRATIONS,
-    Intents.FLAGS.GUILD_PRESENCES,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildIntegrations
   ],
   dirs: [
     join(__dirname, 'commands'),
     join(__dirname, 'events')
   ],
-  failIfNotExists: true,
+	failIfNotExists: true,
   database: new MongoDBProvider(process.env.mongodb_uri),
-  messageSupport: true,
-  messagePrefix: '!',
-  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+	messageSupport: true,
+	messagePrefix: '!',
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 client.queue = new Collection();
 client.db = new Collection();
-client.giveawaysManager = new GiveawaysManager(client, {
-  storage: "./giveaways.json",
-  default: {
-    botsCanWin: false,
-    embedColor: "#FF0000",
-    reaction: "🎉",
-    lastChance: {
-      enabled: true,
-      content: '⚠️ **LAST CHANCE TO ENTER !** ⚠️',
-      threshold: 5000,
-      embedColor: '#FF0000'
-    }
-  }
-});
 
-mongoose.connect(process.env.mongodb_uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(console.log("Success - Connected to MongoDatabase"));
+mongoose.connect(process.env.mongodb_uri).then(console.log("Success - Connected to MongoDatabase"));
 
-client.on('error', console.log);
-client.on('ratelimit', console.log);
-client.on('warn', console.log);
-
-client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
-  console.log(`${member.user.tag} entered giveaway #${giveaway.messageId} (${reaction.emoji.name})`);
-});
-
-client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
-  console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageId} (${reaction.emoji.name})`);
-});
-
-client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
-  console.log(`Giveaway #${giveaway.messageId} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
-});
+client.rest.on('error', console.log);
+client.rest.on('rateLimited', console.log);
+client.rest.on('warn', console.log);
 
 client.login(process.env.token);
+
+/*
+* I'll love the light for it shows me the way, yet I'll endure the darkness because it shows me the stars.
+*
+* @AUTHOR
+* Whattyu
+*/
