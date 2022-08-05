7384287f9f2500b83b8ac51d653037455df7b74d
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-08-05 18:25:50 +0300

    Update to V14

--- src/bot/events/interactionCreate.js
+++ src/bot/events/interactionCreate.js
@@ -1,11 +1,12 @@
 const { Listener } = require("gcommands")
 const { search } = require("youtube-sr").default;
+const { InteractionType } = require("discord.js")
 
 new Listener({
   name: "interactionCreate",
   event: "interactionCreate",
   run: async(interaction) => {
-    if(interaction.isAutocomplete()) {
+    if(interaction.type === InteractionType.ApplicationCommandAutocomplete) {
       let query = interaction.options.getString('query', true);
       const videos = await search(query || 'Never gonna give you up', { limit: 25 });
 
